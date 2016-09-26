6a48e0a3065870fff6b25c7cbfe35c3f79b4b3c2
aliouswang <wsw622@126.com>
2016-09-26 21:42:23 +0800

    rxjava simple

--- library/src/main/java/com/alious/rxjava/library/DeferSimple.java
+++ library/src/main/java/com/alious/rxjava/library/DeferSimple.java
@@ -1,5 +1,7 @@
 package com.alious.rxjava.library;
 
+import java.util.concurrent.TimeUnit;
+
 import rx.Observable;
 import rx.Subscriber;
 
@@ -9,43 +11,56 @@ import rx.Subscriber;
 
 public class DeferSimple {
 
+    static long count = 0;
+
     public static void main(String[] args) {
-        SomeType instance = new SomeType();
-        Observable<String> value = instance.valueObservable();
-        instance.setValue("Some value");
-        value.subscribe(new Subscriber<String>() {
+//        SomeType instance = new SomeType();
+//        Observable<String> value = instance.valueObservable();
+//        instance.setValue("Some value");
+//        value.subscribe(new Subscriber<String>() {
+//            @Override
+//            public void onCompleted() {
+//
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(String s) {
+//                System.out.println(s);
+//            }
+//        });
+
+        new Thread(new Runnable() {
             @Override
-            public void onCompleted() {
+            public void run() {
+                Observable.interval(1, TimeUnit.SECONDS).subscribe(new Subscriber<Long>() {
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("interval onCompleted");
+                    }
 
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        count = aLong;
+                        System.out.println("interval onNext");
+                    }
+                });
             }
+        }).start();
 
-            @Override
-            public void onError(Throwable e) {
+        while(count < 10) {
+            Thread.yield();
+        }
 
-            }
-
-            @Override
-            public void onNext(String s) {
-                System.out.println(s);
-            }
-        });
-
-        Observable.error(new RuntimeException()).subscribe(new Subscriber<Object>() {
-            @Override
-            public void onCompleted() {
-                System.out.println("onCompleted");
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                System.out.println("onError");
-            }
-
-            @Override
-            public void onNext(Object s) {
-                System.out.println("onNext");
-            }
-        });
     }
 
 }
